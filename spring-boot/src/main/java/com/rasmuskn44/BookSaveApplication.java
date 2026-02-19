3372494d507e19821ac9c7e482daa26c098ed0d6
rasmuskn44 <rasmus.knoop@gmx.de>
2026-02-19 14:12:54 +0100

    Generate spring-boot files

---
+++ spring-boot/src/main/java/com/rasmuskn44/BookSaveApplication.java
@@ -0,0 +1,13 @@
+package com.rasmuskn44;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSaveApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/com/rasmuskn44/BookSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmuskn44;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
